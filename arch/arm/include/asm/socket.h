6f0a63744facb94726decac0ae848d6db117788b
Eric Dumazet <edumazet@google.com>
2013-09-24 08:20:52 -0700

    net: introduce SO_MAX_PACING_RATE
    As mentioned in commit afe4fd062416b ("pkt_sched: fq: Fair Queue packet
    scheduler"), this patch adds a new socket option.
    
    SO_MAX_PACING_RATE offers the application the ability to cap the
    rate computed by transport layer. Value is in bytes per second.
    
    u32 val = 1000000;
    setsockopt(sockfd, SOL_SOCKET, SO_MAX_PACING_RATE, &val, sizeof(val));
    
    To be effectively paced, a flow must use FQ packet scheduler.
    
    Note that a packet scheduler takes into account the headers for its
    computations. The effective payload rate depends on MSS and retransmits
    if any.
    
    I chose to make this pacing rate a SOL_SOCKET option instead of a
    TCP one because this can be used by other protocols.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Steinar H. Gunderson <sesse@google.com>
    Cc: Michael Kerrisk <mtk.manpages@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: Iea51b6104a5420d5c9ed1ea7382cbd53bdb8f4be

--- arch/arm/include/asm/socket.h
+++ arch/arm/include/asm/socket.h
@@ -68,6 +68,7 @@
 
 /* Instruct lower device to use last 4-bytes of skb data as FCS */
 #define SO_NOFCS		43
+#define SO_MAX_PACING_RATE	44
 
 #define SO_SET_DOMAIN_NAME 51
 #endif /* _ASM_SOCKET_H */
